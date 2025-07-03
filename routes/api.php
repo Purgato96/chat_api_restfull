742e3c485eb3c39748baeb77fa0373171e583fcc
Purgato96 <matheus_purgato@hotmail.com>
2025-07-03 11:36:14 -0300

    testando novas implementações

--- routes/api.php
+++ routes/api.php
@@ -99,4 +99,6 @@ Route::fallback(function () {
         'documentation' => '/api/v1/status'
     ], 404);
 });
-
+Route::prefix('v1')->group(function () {
+    Route::post('/auth/chatrace-login', [\App\Http\Controllers\Api\AuthController::class, 'chatraceLogin']);
+});
