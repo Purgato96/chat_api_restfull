2df842039201332bd8254edb0c24c3c54fa279dd
Purgato96 <matheus_purgato@hotmail.com>
2025-07-17 14:13:55 -0300

    Correções aplicadas com sucesso

--- resources/js/pages/Chat/Room.vue
+++ resources/js/pages/Chat/Room.vue
@@ -1,6 +1,6 @@
 <template>
     <ChatLayout :title="`Sala: ${room.name}`">
-        <div class="py-12">
+        <div class="bg-blue-50 py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                     <!-- Header da sala -->
